c5b5ee127a7d152745b387092aceae556659947f
Denys Yeremenko <95313654+DenysYeremenko@users.noreply.github.com>
2022-06-07 23:04:41 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryEl = document.querySelector('.gallery');
+
+const galerryImgAdd = array => {
+    array.map(({preview, original, description}) => {
+        const newImageEl = document.createElement('img');
+            newImageEl.src = preview
+            newImageEl.alt = description
+            newImageEl.classList.add('gallery__image')
+            
+
+        const newLinkEl = document.createElement('a')
+            newLinkEl.href = original
+            newLinkEl.classList.add("gallery__item")
+            newLinkEl.append(newImageEl)
+
+        galleryEl.append(newLinkEl)
+    }) 
+}
+
+galerryImgAdd(galleryItems)
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 // Change code below this line
 
 console.log(galleryItems);
